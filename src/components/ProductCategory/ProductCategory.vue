47749c7a9c46dfb754d228cf15fdd4d609d2d9ed
Foysal Mahmood <foysalcse033@gmail.com>
2025-01-03 00:01:32 +0600

    feat: add sub sub category

--- src/components/ProductCategory/ProductCategory.vue
+++ src/components/ProductCategory/ProductCategory.vue
@@ -13,7 +13,7 @@ const handleCategoryList = async () => {
     const result = await getCategories();
     categoryList.value = result.data?.list_data;
   }
-  catch(error) {
+  catch (error) {
     console.log(error);
   }
 };
@@ -24,21 +24,39 @@ const handleCategoryList = async () => {
     <div class="head">Browse Categories</div>
     <ul class="main-categories sidebar-style">
       <li v-for="(item, index) in categoryList || []" :key="index" class="main-nav-list">
-        <a class="d-flex align-items-center" data-bs-toggle="collapse" :href="`#collapse-${index}`"
+        <a class="d-flex align-items-center ms-1" data-bs-toggle="collapse" :href="`#collapse-${index}`"
           :aria-expanded="false" :aria-controls="`collapse-${index}`">
-          {{ item?.par_cat_name }} <span class="ms-1">(2)***</span>
+          {{ item?.par_cat_name }}
+          <span class="ms-1">(2)***</span>
         </a>
         <ul class="collapse" :id="`collapse-${index}`" :aria-labelledby="`collapse-${index}`"
           data-bs-parent=".main-nav-list">
-          <li v-for="(subItem, subIndex) in item?.sub_categories" :key="subIndex" class="main-nav-list child">
-              <a href="#" class="d-flex align-items-center">
-                {{ subItem?.sub_cat_name }} <span class="ms-1">(2)***</span>
-              </a>
+          <li v-for="(subItem, subIndex) in item?.sub_categories || []" :key="subIndex" class="main-nav-list child">
+            <a class="d-flex align-items-center ms-2" data-bs-toggle="collapse"
+              :href="`#collapse-sub-${index}-${subIndex}`" :aria-expanded="false"
+              :aria-controls="`collapse-sub-${index}-${subIndex}`">
+              {{ subItem?.sub_cat_name }}
+              <span class="ms-1">(2)***</span>
+            </a>
+            <ul class="collapse" :id="`collapse-sub-${index}-${subIndex}`"
+              :aria-labelledby="`collapse-sub-${index}-${subIndex}`">
+              <li v-for="(subSubItem, subSubIndex) in subItem?.sub_sub_categories || []" :key="subSubIndex"
+                class="main-nav-list child">
+                <a href="#" class="d-flex align-items-center ms-3">
+                  {{ subSubItem?.sub_sub_cat_name }}
+                  <span class="ms-1">(2)***</span>
+                </a>
+              </li>
+            </ul>
           </li>
         </ul>
       </li>
     </ul>
   </div>
+
+
+
+
 </template>
 
 <style scoped src="./ProductCategory.css"></style>
